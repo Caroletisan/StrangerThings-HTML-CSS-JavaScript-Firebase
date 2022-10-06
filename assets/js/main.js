764f295dcf3a81491a3f823a6c224ed3e8ab5318
Caroletisan <carolsantos_leticia@hotmail.com>
2022-10-06 09:12:46 -0300

    const

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+
+const txtName = document.getElementById('txtName')
+const txtEmail = getElementById('txtEmail')
+const txtLevel = getElementById('txtLevel')
+const txtCharacter = getElementById('txtCharacter')
+const btnSubscribe = getElementById('btnSubscribe')
